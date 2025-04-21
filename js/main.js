84f4586572daa80817ec588596823158fe6acef8
lbascl2 <lbascl2@lsu.edu>
2025-04-21 14:04:28 -0500

    Established working, responsive navigation header
    working and responsive navigation with changing hamburger.
    
    working on footer now

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // JS scripts placed here
+
+const menuToggle = document.querySelector('.hamburger');
+const menu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+  const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+  menuToggle.setAttribute('aria-expanded', !expanded);
+  menu.classList.toggle('expanded');
+  menuToggle.classList.toggle('is-active'); // Animation toggle
+});
